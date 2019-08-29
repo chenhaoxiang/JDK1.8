44bd5d7de6fb0f57c46d19bc25ae3e74c85eb4fa
chenhaoxiang <uifuture@uifuture.com>
2019-08-29 10:38:23 +0800

    docs：AbstractList类equals(Object o)

--- jdk-analyze/src/main/java/java/util/AbstractList.java
+++ jdk-analyze/src/main/java/java/util/AbstractList.java
@@ -460,40 +460,32 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
     // Comparison and hashing
 
     /**
-     * Compares the specified object with this list for equality.  Returns
-     * {@code true} if and only if the specified object is also a list, both
-     * lists have the same size, and all corresponding pairs of elements in
-     * the two lists are <i>equal</i>.  (Two elements {@code e1} and
-     * {@code e2} are <i>equal</i> if {@code (e1==null ? e2==null :
-     * e1.equals(e2))}.)  In other words, two lists are defined to be
-     * equal if they contain the same elements in the same order.<p>
-     *
-     * This implementation first checks if the specified object is this
-     * list. If so, it returns {@code true}; if not, it checks if the
-     * specified object is a list. If not, it returns {@code false}; if so,
-     * it iterates over both lists, comparing corresponding pairs of elements.
-     * If any comparison returns {@code false}, this method returns
-     * {@code false}.  If either iterator runs out of elements before the
-     * other it returns {@code false} (as the lists are of unequal length);
-     * otherwise it returns {@code true} when the iterations complete.
+     * 比较两个集合是否相等
      *
      * @param o the object to be compared for equality with this list
      * @return {@code true} if the specified object is equal to this list
      */
     public boolean equals(Object o) {
-        if (o == this)
+        if (o == this) {
+            //引用和引用比较，当然是相等啦
             return true;
-        if (!(o instanceof List))
+        }
+        if (!(o instanceof List)) {
+            //不是List集合，肯定就是false，类型都不一样，怎么能相等
             return false;
+        }
 
         ListIterator<E> e1 = listIterator();
         ListIterator<?> e2 = ((List<?>) o).listIterator();
+        //遍历两个集合进行比较，每个元素，每个位置都需要一样的
         while (e1.hasNext() && e2.hasNext()) {
             E o1 = e1.next();
             Object o2 = e2.next();
-            if (!(o1==null ? o2==null : o1.equals(o2)))
+            if (!(o1==null ? o2==null : o1.equals(o2))) {
                 return false;
+            }
         }
+        //最后，长度也要一样！
         return !(e1.hasNext() || e2.hasNext());
     }
 
